1f48fc94f54a059eefc0c7786f36887fa9da3180
miketheshadow <miketheshadow1@gmail.com>
2022-09-16 17:44:17 -0600

    Attempting to broadcast

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,46 @@
-use std::env;
+//! Perform a Bluetooth LE advertisement.
 
-async fn query_adapter(adapter: &bluer::Adapter) -> bluer::Result<()> {
-    println!("    Address:                    {}", adapter.address().await?);
-    println!("    Friendly name:              {}", adapter.alias().await?);
-    println!("    Modalias:                   {:?}", adapter.modalias().await?);
-    println!("    Powered:                    {:?}", adapter.is_powered().await?);
-    println!("    Discoverabe:                {:?}", adapter.is_discoverable().await?);
-    println!("    Pairable:                   {:?}", adapter.is_pairable().await?);
-    println!("    UUIDs:                      {:?}", adapter.uuids().await?);
-    println!();
-    println!("    Active adv. instances:      {}", adapter.active_advertising_instances().await?);
-    println!("    Supp.  adv. instances:      {}", adapter.supported_advertising_instances().await?);
-    println!("    Supp.  adv. includes:       {:?}", adapter.supported_advertising_system_includes().await?);
-    println!("    Adv. capabilites:           {:?}", adapter.supported_advertising_capabilities().await?);
-    println!("    Adv. features:              {:?}", adapter.supported_advertising_features().await?);
-
-    Ok(())
-}
-
-async fn query_all_adapter_properties(adapter: &bluer::Adapter) -> bluer::Result<()> {
-    let props = adapter.all_properties().await?;
-    for prop in props {
-        println!("    {:?}", &prop);
-    }
-    Ok(())
-}
+use bluer::adv::Advertisement;
+use std::time::Duration;
+use bluer::Adapter;
+use tokio::{
+    io::{AsyncBufReadExt, BufReader},
+    time::sleep,
+};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> bluer::Result<()> {
-    let all_properties = env::args().any(|arg| arg == "--all-properties");
-
+    env_logger::init();
     let session = bluer::Session::new().await?;
-    let adapter_names = session.adapter_names().await?;
-    for adapter_name in adapter_names {
-        println!("Bluetooth adapater {}:", &adapter_name);
-        let adapter = session.adapter(&adapter_name)?;
-        adapter.set_pairable(true).await?;
-        adapter.set_discoverable(true).await?;
-        if all_properties {
-            query_all_adapter_properties(&adapter).await?;
-        } else {
-            query_adapter(&adapter).await?;
-        }
-        println!();
+    let adapter = session.default_adapter().await?;
+    let test_adapter : Result<Adapter, E> = session.adapter("music-server-controller").await;
+    if test_adapter.is_ok() {
+        println!("Found it by name!");
+    } else {
+        println!("Did not find it by name!");
     }
+
+    adapter.set_powered(true).await?;
+
+    println!("Advertising on Bluetooth adapter {} with address {}", adapter.name(), adapter.address().await?);
+    let le_advertisement = Advertisement {
+        advertisement_type: bluer::adv::Type::Peripheral,
+        service_uuids: vec!["123e4567-e89b-12d3-a456-426614174000".parse().unwrap()].into_iter().collect(),
+        discoverable: Some(true),
+        local_name: Some("le_advertise".to_string()),
+        ..Default::default()
+    };
+    println!("{:?}", &le_advertisement);
+    let handle = adapter.advertise(le_advertisement).await?;
+
+    println!("Press enter to quit");
+    let stdin = BufReader::new(tokio::io::stdin());
+    let mut lines = stdin.lines();
+    let _ = lines.next_line().await;
+
+    println!("Removing advertisement");
+    drop(handle);
+    sleep(Duration::from_secs(1)).await;
+
     Ok(())
 }
